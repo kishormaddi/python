254e5e9c7473d13f27d5df0384e0bee71d99ba31
Kishor Maddi <45893934+kishormaddi@users.noreply.github.com>
2018-12-19 13:04:15 +0530

    Add files via upload

---
+++ Python Practice.py
@@ -0,0 +1,110 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+print ('kishor')
+
+
+# In[6]:
+
+
+my_dict = {'name':'kishor', 'age':35}
+
+
+# In[7]:
+
+
+my_dict['address'] = 'Hyderabad'
+
+
+# In[8]:
+
+
+my_dict
+
+
+# In[9]:
+
+
+my_dict['age']
+
+
+# In[11]:
+
+
+my_dict.pop('age')
+
+
+# In[12]:
+
+
+my_dict
+
+
+# In[13]:
+
+
+my_dict.popitem()
+
+
+# In[14]:
+
+
+my_dict
+
+
+# In[15]:
+
+
+del my_dict
+
+
+# In[16]:
+
+
+my_dict
+
+
+# In[24]:
+
+
+my_dict = {1:'kishor', 'age': '36'}
+
+
+# In[18]:
+
+
+my_dict['age']
+
+
+# In[19]:
+
+
+my_dict.clear
+
+
+# In[20]:
+
+
+my_dict
+
+
+# In[21]:
+
+
+my_dict.clear()
+
+
+# In[25]:
+
+
+my_dict.items()
+
+
+# In[42]:
+
+
+marks = {}.fromkeys(['Math', 'English', 'Science'],0)
+
